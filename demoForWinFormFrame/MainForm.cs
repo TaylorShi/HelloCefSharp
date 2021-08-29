1a691deadec85bf0292782460ea9d470a2a21e1d
YongAn <414675056@qq.com>
2021-08-30 00:22:34 +0800

    winforms添加cefSharp

--- demoForWinFormFrame/MainForm.cs
+++ demoForWinFormFrame/MainForm.cs
@@ -8,11 +8,17 @@ using System.Windows.Forms;
 
 namespace demoForWinFormFrame
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+            Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Browser.Load("www.bing.com");
         }
     }
 }
--- demoForWinFormFrame/Program.cs
+++ demoForWinFormFrame/Program.cs
@@ -14,7 +14,7 @@ namespace demoForWinFormFrame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main2Form());
         }
     }
 }
